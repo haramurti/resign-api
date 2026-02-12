56b316f9e4889c03bdf2dcd94907a639328cbc8c
nevalhr <phneval@gmail.com>
2026-02-12 18:47:28 +0700

    feat: interface , behaviour in resign.go and leave.go

--- internal/domain/resign.go
+++ internal/domain/resign.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"context"
 	"time"
 
 	"gorm.io/gorm"
@@ -13,4 +14,18 @@ type Resignation struct {
 	ResignDate time.Time `gorm:"not null" json:"resign_date"`
 	Reason     string    `gorm:"type:text;not null" json:"reason"`
 	Status     string    `gorm:"type:varchar(20);default:'pending'" json:"status"`
+}
+
+type ResignationRepository interface {
+	Create(ctx context.Context, resign *Resignation) error
+	FetchAll(ctx context.Context) ([]Resignation, error)
+	GetByID(ctx context.Context, id uint) (Resignation, error)
+	UpdateStatus(ctx context.Context, id uint, status string) error
+}
+
+type ResignationUsecase interface {
+	Submit(ctx context.Context, resign *Resignation) error
+	GetHistory(ctx context.Context) ([]Resignation, error)
+	Approve(ctx context.Context, id uint) error
+	Reject(ctx context.Context, id uint) error
 }
